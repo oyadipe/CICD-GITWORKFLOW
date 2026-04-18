066d2c992bf853bd651156c8273e35a671347cd6
oyadipe <oluwagbenga.y@gmail.com>
2026-04-18 17:31:53 -0400

    deburging

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "google" {
 
 resource "google_storage_bucket" "my_bucket" {
   project                  = "ancient-chiller-433403-k9"
-  name                     = "19031982-terraform-bucket"
+  name                     = "19031982-my_bucket"
   location                 = "us-central1"
   force_destroy            = true
   public_access_prevention = "false"
