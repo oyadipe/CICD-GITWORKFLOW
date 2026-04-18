a54c40ca15a46048eaab46b8815e07d0e1322541
oyadipe <oluwagbenga.y@gmail.com>
2026-04-18 17:54:46 -0400

    corrected by copilot

--- main.tf
+++ main.tf
@@ -12,9 +12,11 @@ provider "google" {
 }
 
 resource "google_storage_bucket" "my_bucket" {
-  project                  = "ancient-chiller-433403-k9"
-  name                     = "19031982-my_bucket"
-  location                 = "us-central1"
-  force_destroy            = true
-  
+  project       = "ancient-chiller-433403-k9"
+  name          = "19031982-my_bucket"
+  location      = "us-central1"
+  force_destroy = true
+
+  # REQUIRED for provider v7.x to avoid the "false is not a valid value" error
+  uniform_bucket_level_access = true
 }
