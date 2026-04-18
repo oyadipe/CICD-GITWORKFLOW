beeddc5f65cf9e2acc265514ff820ef29549583a
oyadipe <oluwagbenga.y@gmail.com>
2026-04-18 16:20:11 -0400

    trying to correct the yaml file

--- main.tf
+++ main.tf
@@ -16,5 +16,5 @@ resource "google_storage_bucket" "my_bucket" {
   name                     = "19031982-terraform-bucket"
   location                 = "us-central1"
   force_destroy            = true
-  public_access_prevention = false
+  public_access_prevention = "disable"
 }
